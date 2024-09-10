9df9c3993a60733e907f738b3e41acc49d0f10da
Juliano Vaz <julianoctvaz@gmail.com>
2024-09-09 22:20:06 -0300

    Creates @state properties and computed filter var

--- CustomSearchBar/ContentView.swift
+++ CustomSearchBar/ContentView.swift
@@ -8,6 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText: String = ""
+    @State private var names: [String] = [
+        "Juliana",
+        "Francis",
+        "Michael Jackson",
+        "Leibniz",
+        "Matheus",
+        "Alana",
+        "Lula",
+        "Michael (The Office)"
+    ]
+
+    private var filteredNames: [String] {
+        if searchText.isEmpty {
+            return names
+        } else {
+            return names.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
