8345f2bfc3ab45ddb5c2f928b1fd25c1515bf715
Juliano Vaz <julianoctvaz@gmail.com>
2024-09-09 21:53:30 -0300

    Initial Commit

---
+++ CustomSearchBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSearchBar
+//
+//  Created by Juliano on 09/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSearchBar/CustomSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchBarApp.swift
+//  CustomSearchBar
+//
+//  Created by Juliano on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
